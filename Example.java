d4aa0782bf6ec77281a306d699771437758f4774
Subodh Nandi <subodhnandi59428@gmail.com>
2025-06-17 10:17:29 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,19 @@
+package com.subodh.Multithreading;
+
+import java.util.Scanner;
+
+public class Example {
+
+	static void m1() {
+		System.out.println("m1");
+		Scanner sc=new Scanner(System.in);
+		sc.next();
+	}
+	static void m2() {
+		System.out.println("m2");
+	}
+	static void m3() {
+		System.out.println("m3");
+	}
+
+}
